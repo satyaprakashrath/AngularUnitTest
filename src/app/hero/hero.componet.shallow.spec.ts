74895062618dacf9ff5f19705fabd9b99a99c7ba
satya <satyaprakash.rath@clarivate.com>
2022-05-28 19:37:40 +0530

    used debug element in the test

--- src/app/hero/hero.componet.shallow.spec.ts
+++ src/app/hero/hero.componet.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { HeroComponent } from "./hero.component";
 
 describe('Hero Component Shalow test', () =>{
@@ -22,5 +23,11 @@ describe('Hero Component Shalow test', () =>{
         fixture.componentInstance.hero = { id:1, name:'SuperDude', strength: 3}
         fixture.detectChanges();
         expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+    })
+
+    it('should have the correct hero in the anchor tag with debug element', ()=>{
+        fixture.componentInstance.hero = { id:1, name:'SuperDude', strength: 3}
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('SuperDude');
     })
 })
